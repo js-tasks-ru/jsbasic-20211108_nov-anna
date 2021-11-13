4efb003cd5d4822a33b7987c80f441a57cf32b4b
Anna Eliseeva <nov-anna@yandex.ru>
2021-11-13 21:52:00 +0300

    найден факториал с помощью двух разных циклов for и while

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+  first = 1;
+  for(i=1; i<=n; i++){
+    first = first * i;
+  }
+  return first;
 }
+console.log(factorial(10));
+
+
+function factorial2(m){
+  var total = 1;
+  while(m){
+    total *= m--;
+  }
+  return total;
+}
+console.log(factorial2(10));
