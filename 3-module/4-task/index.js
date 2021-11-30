a2d6a950eb46d521ed427fca6005fc103bbc5698
Anna Eliseeva <nov-anna@yandex.ru>
2021-11-30 15:50:42 +0300

    переделаны задачи модуль 3, 1 и 4 задача

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,9 +1,6 @@
 function showSalary(users, age) {
-  let string = ""; 
-  users.forEach((item)=>{ 
-    if (item.age <= age) { 
-      string = string + item.name + ", " + item.balance + "\n"; 
-    }
-  });
-  return string.slice(0, -1); 
-}
+    return users
+      .filter(item => item.age <= age)
+      .map(item => `${item.name}, ${item.balance}`)
+      .join('\n');
+  }
